ca08c857b5684279cd1f67c7e0523dbdc8410e1e
Chenyi (Lawrence) Wang <chenyiw@arizona.edu>
2025-07-23 11:50:13 -0700

    Update traffic jam data download

--- scripts/data_model_download.sh
+++ scripts/data_model_download.sh
@@ -11,14 +11,20 @@ curl -L -o ../assets/opv2v_test_split.zip "https://arizona.box.com/shared/static
 echo "Downloading pre-trained model weights..."
 curl -L -o ../assets/model_weights.zip "https://arizona.box.com/shared/static/kdcvfnqapcln931y7jjetw589sf81rtj.zip"
 
+# Download the traffic jam dataset
+echo "Downloading traffic jam dataset..."
+curl -L -o ../assets/traffic_jam_dataset.zip "https://arizona.box.com/shared/static/wzgwvero68pp1t7ulx9ypt9xyj0r6r7l.zip"
+
 # Unzip the files
 echo "Unzipping files..."
 unzip ../assets/opv2v_test_split.zip -d ../assets/
 unzip ../assets/model_weights.zip -d ../assets/
+unzip ../assets/traffic_jam_dataset.zip -d ../assets/
 
 # Remove the zip files
 echo "Removing zip files..."
 rm ../assets/opv2v_test_split.zip
 rm ../assets/model_weights.zip
+rm ../assets/traffic_jam_dataset.zip
 
 echo "Done!"
